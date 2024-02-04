050c735d23296e5a06fcb3867daf99bab3129452
geoweh <georg.wehrhahn@googlemail.com>
2024-02-04 19:37:06 +0100

    kommentiert

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,10 @@
+# Definiert eine Klasse namens 'Question'.
 class Question:
-	def __init__(self, prompt, answer):
-		self.prompt = prompt
-		self.answer = answer
-
-
+    # Der Konstruktor __init__ wird aufgerufen, wenn ein neues 'Question'-Objekt erstellt wird.
+    # Er initialisiert die Objekteigenschaften mit den spezifizierten Werten.
+    def __init__(self, prompt, answer):
+        # 'self' repräsentiert das Objekt, das gerade erstellt wird.
+        # Hier weisen wir den übergebenen Werten den Eigenschaften des Objekts zu.
+        
+        self.prompt = prompt  # 'prompt' ist der Text der Frage, inklusive der Antwortmöglichkeiten.
+        self.answer = answer  # 'answer' ist der Buchstabe, der die korrekte Antwort repräsentiert.
